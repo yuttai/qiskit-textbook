cbc41555b1df2b71d59ddafa0fac199ec72696da
kthfan <3999932@gmail.com>
2022-11-24 17:06:39 +0800

    modify state_vector_exercise into four element vector

--- qiskit-textbook-src/qiskit_textbook/widgets/__init__.py
+++ qiskit-textbook-src/qiskit_textbook/widgets/__init__.py
@@ -44,28 +44,28 @@ def binary_widget(nbits=5):
 def state_vector_exercise(target):
     output = _pre()
     button = widgets.Button(description="Check", layout=widgets.Layout(width='5em'))
-    text_input = widgets.Text(value='[1, 0]',
+    text_input = widgets.Text(value='[1, 0, 0, 0]',
                               placeholder='Type something',
                               width='50px',
                               disabled=False)
 
-    label = widgets.Label(value="State Vector:")
+    label = widgets.Label(value="Enter The State Vector:")
 
     def on_button_click(b):
         try:
             state_vector = text_input.value.strip("[]").replace(" ", "").split(",")
-            c1, c2 = numexpr.evaluate(state_vector[0]), numexpr.evaluate(state_vector[1])
+            c1, c2, c3, c4 = numexpr.evaluate(state_vector[0]), numexpr.evaluate(state_vector[1]), numexpr.evaluate(state_vector[2]), numexpr.evaluate(state_vector[3])
         except Exception as e:
             output.value = str(e).split("(")[0]
             return
 
-        squared_magnitude = abs(c1)**2 + abs(c2)**2
+        squared_magnitude = abs(c1)**2 + abs(c2)**2 + abs(c3)**2 + abs(c4)**2
         p = abs(c1)**2
         if not (squared_magnitude < 1.01 and squared_magnitude > .99): # Close Enough
             output.value = "Magnitude is not equal to 1"
             return
         elif p > target*0.99 and p < target*1.01:
-            output.value = "Correct!"
+            output.value = f"Correct! The answer is :{c1}"
         else:
             output.value = "The absolute value of " + str(c1) + ", squared is not equal to " + str(target)
 
